24b571d21b8e993f917ff7ae37e5680f6b64db8f
RyderBlack <ryhad.boughanmi@gmail.com>
2018-08-07 15:18:03 +0200

    first component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Compo1Component } from './compo1/compo1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Compo1Component
   ],
   imports: [
     BrowserModule
