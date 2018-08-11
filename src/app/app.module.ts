a388fa47bec92fb55713ce1e3b9d85db59f3b996
RyderBlack <ryhad.boughanmi@gmail.com>
2018-08-11 20:26:21 +0200

    updated app for routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,13 @@ import { FormsModule } from '@angular/forms';
 import { AppareilService } from './services/appareil.service';
 import { AuthComponent } from './auth/auth.component';
 import { AppareilViewComponent } from './appareil-view/appareil-view.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'appareils', component: AppareilViewComponent },
+  { path: 'auth', component: AuthComponent },
+  { path: '', component: AppareilViewComponent }
+];
 
 @NgModule({
   declarations: [
@@ -19,7 +26,8 @@ import { AppareilViewComponent } from './appareil-view/appareil-view.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     AppareilService
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -5,11 +5,25 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss']
 })
-export class AuthComponent implements OnInit {
 
-  constructor() { }
+export class AuthService {
 
-  ngOnInit() {
+  isAuth = false;
+
+  signIn() {
+    return new Promise(
+      (resolve, reject) => {
+        setTimeout(
+          () => {
+            this.isAuth = true;
+            resolve(true);
+          }, 2000
+        );
+      }
+    );
   }
 
+  signOut() {
+    this.isAuth = false;
+  }
 }
