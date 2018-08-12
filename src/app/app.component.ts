b05dc5ab825520afc4970b01522698b8d2cccb77
RyderBlack <ryhad.boughanmi@gmail.com>
2018-08-12 10:22:15 +0200

    observable created in app comp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-//import 'rxjs/add/observable/interval';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'ang6-OC';
 
   secondes: Number;
+  counterSubscription: Subscription;
 
   ngOnInit() {
-    const counter = Observable.interval(1000);
-    counter.subscribe(
+    const counter = interval(1000);
+    this.counterSubscription = counter.subscribe(
       (value) => {
         this.secondes = value;
       },
@@ -26,4 +27,12 @@ export class AppComponent implements OnInit {
       }
     );
   }
+
+  ngOnDestroy() {
+    if(this.secondes === 10) {
+      this.counterSubscription.unsubscribe();
+    }
+    
+  }
+
 }
