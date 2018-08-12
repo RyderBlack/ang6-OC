d6c9aa7131a98e3efd28b60920ecfaca1f2108df
RyderBlack <ryhad.boughanmi@gmail.com>
2018-08-12 10:04:23 +0200

    observable created in app comp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Observable } from 'rxjs';
+//import 'rxjs/add/observable/interval';
 
 @Component({
   selector: 'app-root',
@@ -9,11 +10,20 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'ang6-OC';
 
-  constructor() {
-    
-  }
+  secondes: Number;
 
   ngOnInit() {
- 
+    const counter = Observable.interval(1000);
+    counter.subscribe(
+      (value) => {
+        this.secondes = value;
+      },
+      (error) => {
+        console.log('Uh-oh, an error occurred! : ' + error);
+      },
+      () => {
+        console.log('Observable complete!');
+      }
+    );
   }
 }
